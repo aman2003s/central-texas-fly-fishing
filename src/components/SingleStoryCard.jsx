18b26ff25ec17f16f86e98f0e57223333d181b8a
aman2003s <itzamanksingh@gmail.com>
2025-03-11 00:44:44 +0530

    Css conflict fix

--- src/components/SingleStoryCard.jsx
+++ src/components/SingleStoryCard.jsx
@@ -4,13 +4,13 @@ import "./css/singleStoryCard.css";
 const SingleStoryCard = ({ userImg, heading, description }) => {
   return (
     <div className="story-card">
-      <div className="image-container mob-hide">
+      <div className="image-container mob-single-hide">
         <img src={userImg} />
       </div>
       <div className="text-container">
         <h4 className="title">{heading}</h4>
         <div className="story-mob-display">
-          <img src={userImg} className="mob-show mob-img"/>
+          <img src={userImg} className="mob-single-show mob-img" />
           <p className="description">{description}</p>
         </div>
         <button className="read-more-button">Read more</button>
